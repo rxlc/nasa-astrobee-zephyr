8bcab9437964372df0142a52bd8effa50a02dc7d
rxlc <110650273+rxlc@users.noreply.github.com>
2023-06-25 17:47:37 -0700

    Update ArTag.java

--- ArTag.java
+++ ArTag.java
@@ -1,5 +1,6 @@
 package jp.jaxa.iss.kibo.rpc.testing;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,27 +40,6 @@ public class ArTag {
         catch (Exception e) {
             Log.i("IdMatrix", "Method failed");
         }
-        /*
-        try {
-            for (int i = 0; i < ids.rows(); i++) {
-                Log.i("IdMatrix", "Rows" + i);
-                for (int j = 0; j < ids.cols(); j++) {
-                    Log.i("IdMatrix", "Cols" + j);
-                    if (ids.get(i,j) != null) {
-                        for (int k = 0; k < ids.get(i,j).length; k++) {
-                            Log.i("IdMatrix", "k" + k);
-                            Log.i("IdMatrix", "" + ids.get(i,j)[k]);
-                        }
-                    } else {
-                        Log.i("IdMatrix", "null");
-                    }
-                }
-            }
-        }
-        catch (Exception e) {
-            Log.i("IdMatrix", "Method failed");
-        }
-        */
     }
 
     public static void logCorners(List<Mat> corners) {
@@ -85,51 +65,6 @@ public class ArTag {
         } catch (Exception e) {
             Log.i("CornerMatrix", "Method 1 error");
         }
-
-        /*
-        Log.i("CornerMatrix", "Second iteration method");
-        try {
-            for (int x = 0; x < corners.size(); x++) {
-                Log.i("CornerMatrix", "x" + x);
-                for (int i = 0; i < corners.get(x).rows(); i++) {
-                    Log.i("CornerMatrix", "i" + i);
-                    for (int j = 0; j < corners.get(x).cols(); j++) {
-                        Log.i("CornerMatrix", "j" + j);
-                        if (corners.get(x).get(i, j) != null) {
-                            for (int k = 0; k < corners.get(x).get(i, j).length; k++) {
-                                Log.i("CornerMatrix", "k" + k);
-                                Log.i("CornerMatrix", "" + corners.get(x).get(i, j)[k]);
-                            }
-                        } else {
-                            Log.i("CornerMatrix", "null");
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            Log.i("CornerMatrix", "Method 2 error");
-
-        }
-
-        Log.i("CornerMatrix", "Third Iteration Method");
-        try {
-            for (int x = 0; x < corners.size(); x++) {
-                Log.i("CornerMatrix", "Marker " + x + "Corners");
-                for (int i = 0; i < corners.get(x).rows(); i++) {
-                    for (int j = 0; j < corners.get(x).cols(); j++) {
-                        double corner[] = corners.get(x).get(i, j);
-                        if (corner != null) {
-                            Log.i("CornerMatrix", "Corner " + i + ", " + j + " : " + corner[0] + ", " + corner[1]);
-                        } else {
-                            Log.i("CornerMatrix", "Corner " + i + ", " + j + " : null");
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            Log.i("CornerMatrix", "Method 3 error");
-        }
-        */
     }
 
     public static List<List<Double>> tags(Mat image, int target_num) {
@@ -146,11 +81,9 @@ public class ArTag {
         double xDif = 0;
         double yDif = 0;
         for (int i = 0; i<ids.height(); i++) { //this is correct, ids is a matrix consisting of a width of 1
-            if (((int)ids.get(i,0)[0]-1)/4 + 1 != target_num) {
-            }
-            else {
+            if (((int)ids.get(i,0)[0]-1)/4 + 1 == target_num) {
                 List<Double> temp = new ArrayList<>();
-                if (ids.get(i, 0) == null) { // I think this was our issue, we switched the positions of height and width
+                if (ids.get(i, 0) == null) {
                     break;
                 }
 
@@ -190,26 +123,20 @@ public class ArTag {
                     temp.set(j, (temp.get(j) - 640) / xDif);
                 }
                 for (int j = 2; j< temp.size(); j+=2) {
-                    temp.set(j, (temp.get(j) - 480) / yDif);
+                    temp.set(j, ((temp.get(j)) - 480) / yDif);
                 }
                 result.add(temp); // for each arraylist in result, it contains 9 values, [id, x1, y1, x2, y2, x3, y3, x4, y4]
 
                 for (Double val : temp) {
                     Log.i("Values of temp", " " + val);
                 }
-
+                //break;
             }
         }
         return result; // result contains 1 to 4 ar tags
     }
 
     public static Mat undistort(Mat image, double[][] info) {
-        for (double[] array : info) {
-            Log.i("Undistortion", "Double Array\n");
-            for (double val : array) {
-                Log.i("Value", " " + val);
-            }
-        }
         Mat K = new Mat(3, 3, CvType.CV_32FC1);
         K.put(0,0, info[0]);
         Mat D = new Mat(1, 5, CvType.CV_32FC1);
@@ -219,7 +146,93 @@ public class ArTag {
         return solution;
     }
 
-    public static Point fireLaser(List<List<Double>> tags, Point currentPoint) {
+    public static double[] findXYDiff(List<List<Double>> tags) {
+        ArrayList<Double> xDiffs = new ArrayList<>();
+        ArrayList<Double> yDiffs = new ArrayList<>();
+
+        for (int n = 0; n < tags.size(); n++) {
+            int pos; // will be the position in which it is in
+            /*
+            [ 2, 1 ]
+            [ 3, 4 ]
+             */
+
+            int tag_num = (int) tags.get(n).remove(0).shortValue(); // id of tha ar tag
+
+            if (tag_num % 4 == 1) {
+                pos = 1;
+            } else if (tag_num % 4 == 2) {
+                pos = 2;
+            } else if (tag_num % 4 == 3) {
+                pos = 3;
+            } else {
+                pos = 4;
+            }
+
+            Log.i("Tag Position", " " + pos);
+
+            double[] xPositions = new double[4];
+            double[] yPositions = new double[4];
+
+            for (int i = 0; i < tags.get(n).size(); i++) {
+                if (i % 2 == 0) {
+                    xPositions[i / 2] = tags.get(n).get(i);
+                    Log.i("XPositions", " " + tags.get(n).get(i));
+                } else {
+                    yPositions[i / 2] = tags.get(n).get(i);
+                    Log.i("YPositions", " " + tags.get(n).get(i));
+                }
+
+            }
+            for (double val : xPositions) {
+                Log.i("xPositions", " " + val);
+            }
+            for (double val : yPositions) {
+                Log.i("yPositions", " " + val);
+            }
+            // Now lets aim the laser at the centre of the ar tag
+            double x_pos = (xPositions[1] + xPositions[0]) * 0.5;
+            double y_pos = (yPositions[3] + yPositions[1]) * 0.5;
+
+            Log.i("positions", "x_pos " + x_pos);
+            Log.i("positions", "y_pos" + y_pos);
+
+            if (pos == 1 || pos == 4) {
+                x_pos -= 0.10;
+            } else {
+                x_pos += 0.10;
+            }
+
+            if (pos == 1 || pos == 2) {
+                y_pos += 0.0375;
+            } else {
+                y_pos -= 0.0375;
+            }
+
+            //adjustment from heart of ar tag to centre of target
+            Log.i("positions", "x_pos " + x_pos);
+            Log.i("positions", "y_pos" + y_pos);
+
+            x_pos -= 0.0994;
+            y_pos += 0.0285;
+
+            xDiffs.add(x_pos);
+            yDiffs.add(y_pos);
+
+            Log.i("positions", "x_pos " + x_pos);
+            Log.i("positions", "y_pos" + y_pos);
+        }
+
+        double xAvg = findAverage(xDiffs);
+        double yAvg = findAverage(yDiffs);
+
+        Log.i("positions", "x_avg " + xAvg);
+        Log.i("positions", "y_avg" + yAvg);
+
+        return new double[]{xAvg, yAvg};
+    }
+
+    public static Point adjustForFiring(double[] xyPos, Point currentPoint, int target_num) {
         double x = currentPoint.getX();
         double y = currentPoint.getY();
         double z = currentPoint.getZ(); //eliminated z element since it does not seem to change target
@@ -227,77 +240,13 @@ public class ArTag {
         Log.i("Current Positions", "yPos " + y);
         Log.i("Current Positions", "zPos " + z);
 
+        double x_pos = xyPos[0];
+        double y_pos = xyPos[1];
 
-        int pos; // will be the position in which it is in
-        int tag_num = (int)tags.get(0).remove(0).shortValue(); // id of tha ar tag
-        if (tag_num % 4 == 1) {
-            pos = 1;
-        }
-        else if (tag_num % 4 == 2) {
-            pos = 2;
-        }
-        else if (tag_num % 4 == 3) {
-            pos = 3;
-        }
-        else {
-            pos = 4;
-        }
-        Log.i("Tag Position", " " + pos);
-
-        double[] xPositions = new double[4];
-        double[] yPositions = new double[4];
-        for (int i = 0; i<tags.get(0).size(); i++) {
-            if (i % 2 == 0) {
-                xPositions[i / 2] = tags.get(0).get(i);
-                Log.i("XPositions", " " + tags.get(0).get(i));
-            }
-            else {
-                yPositions[i/2] = tags.get(0).get(i);
-                Log.i("YPositions", " " + tags.get(0).get(i));
-            }
-
-        }
-        for (double val : xPositions) {
-            Log.i("xPositions", " " + val);
-        }
-        for (double val : yPositions) {
-            Log.i("yPositions", " " + val);
-        }
-        // Now lets aim the laser at the centre of the ar tag
-        double x_pos = (xPositions[1] + xPositions[0])*0.5;
-        double y_pos = (yPositions[3] + yPositions[1])*0.5;
-
-        Log.i("positions", "x_pos " + x_pos);
-        Log.i("positions", "y_pos" + y_pos);
-
-        if (pos == 1 || pos == 4) {
-            x_pos -= 0.10;
-        }
-        else {
-            x_pos += 0.10;
-        }
-        if (pos == 1 || pos == 2) {
-            y_pos += 0.0375;
-        }
-        else {
-            y_pos -= 0.0375;
-        }
-
-        //adjustment from heart of ar tag to centre of target
-        Log.i("positions", "x_pos " + x_pos);
-        Log.i("positions", "y_pos" + y_pos);
-
-
-        x_pos -= 0.0994;
-        y_pos += 0.0285;
-
-        Log.i("positions", "x_pos " + x_pos);
-        Log.i("positions", "y_pos" + y_pos);
         //final adjustments based on laser position
-        int target_num = (tag_num-1) / 4 + 1;
         if (target_num == 2 ) {
             x-= x_pos;
-            y+= y_pos;
+            y-= y_pos;
         }
         else if (target_num == 3) {
             y+=x_pos;
@@ -319,9 +268,75 @@ public class ArTag {
             x+= x_pos;
             z+= y_pos;
         }
+
         Log.i("Final Positions", "xPos " + x);
         Log.i("Final Positions", "yPos " + y);
         Log.i("Final Positions", "zPos " + z);
         return new Point(x,y,z);
+    }
+
+    public static Quaternion adjustRotation(Point target, Point currentPoint, int target_num) {
+        double[] position = {currentPoint.getX() + 0.1302, currentPoint.getY() + 0.0572, currentPoint.getZ() -0.1111};
+
+        double zDif;
+        if (target_num == 2) {
+            zDif = 4.48 - position[2];
+        }
+        else {
+            zDif = 11.355 - position[0];
+        }
+
+        double x_pos;
+        if (target_num == 2) {
+            x_pos = target.getX() - currentPoint.getX();
+        }
+        else {
+            x_pos = target.getY() - currentPoint.getY();
+        }
+
+        double theta = Math.asin(x_pos/zDif);
+
+        Quaternion result;
+        result = eulerToQuaternion(0,90 + Math.toDegrees(theta), 0);
+
+        /*
+        if (target_num == 2) {
+            result = eulerToQuaternion(0,90 + theta, 0);
+        }
+        else {
+            result = new Quaternion(0f, 0f, -1f, (float)Math.cos(theta*0.5));
+        }
+        */
+
+        return result;
+    }
+
+    public static Quaternion eulerToQuaternion(double x, double y, double z) {
+        double yaw = Math.toRadians(z);
+        double pitch = Math.toRadians(y);
+        double roll = Math.toRadians(x);
+
+        double cy = Math.cos(yaw * 0.5);
+        double sy = Math.sin(yaw * 0.5);
+        double cp = Math.cos(pitch * 0.5);
+        double sp = Math.sin(pitch * 0.5);
+        double cr = Math.cos(roll * 0.5);
+        double sr = Math.sin(roll * 0.5);
+
+        double qx = sr * cp * cy - cr * sp * sy;
+        double qy = cr * sp * cy + sr * cp * sy;
+        double qz = cr * cp * sy - sr * sp * cy;
+        double qw = cr * cp * cy + sr * sp * sy;
+
+        return new Quaternion((float) qx, (float) qy, (float) qz, (float) qw);
+    }
+
+    public static double findAverage(ArrayList<Double> arr) {
+        double sum = 0.0;
+        for (int i = 0; i < arr.size(); i++) {
+            sum += arr.get(i);
+        }
+
+        return sum / arr.size();
     }
 }
